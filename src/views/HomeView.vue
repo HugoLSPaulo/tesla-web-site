391fc5447d2baca035944c099a8bf054b318bf28
HugoLSPaulo <64653369+HugoLSPaulo@users.noreply.github.com>
2023-04-07 18:56:31 +0100

    specifications

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,19 @@
 <template>
-  <Promo/>
+  <div>
+     <Promo/>
+  <Specifications/>
+  </div>
 </template>
 
 <script>
   import Promo from '../components/Promo.vue'
+
+  import Specifications from '../components/Specifications.vue'
 export default {
   name: 'HomeView',
   components: {
-    Promo
+    Promo,
+    Specifications
   }
 }
 </script>
