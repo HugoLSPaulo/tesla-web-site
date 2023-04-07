d948cea8976a1416b8e1119120b49d0b9b81f1e8
HugoLSPaulo <64653369+HugoLSPaulo@users.noreply.github.com>
2023-04-07 18:26:53 +0100

    nav bar

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,18 @@
+<template>
+	<span>
+		<v-toolbar app color="black" dark>
+		<v-toolbar-title>
+			<v-img src="../../public/tesla.png" height="110px" width="200px"></v-img>
+		</v-toolbar-title>
+		<v-spacer></v-spacer>
+		<v-btn flat class="hiddem-sm-and-down white font-weight-bold black--text">Specifications</v-btn>
+		<v-btn class="hiddem-sm-and-down white font-weight-bold black--text ml-3">Specifications</v-btn>
+	</v-toolbar>
+	</span>
+</template>
+<script>
+	export default{
+
+	}
+</script>
+<style></style>
