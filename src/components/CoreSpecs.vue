87f9a4295fc0ee1def9d4a9dbda6623c568ca239
HugoLSPaulo <64653369+HugoLSPaulo@users.noreply.github.com>
2023-04-07 19:09:27 +0100

    interior and specifications

---
+++ src/components/CoreSpecs.vue
@@ -0,0 +1,43 @@
+<template>
+  <div style="background-color:black;margin-top:-10px">
+      <div fluid class="background" style="display:table">
+          <div style="display:table-cell;vertical-align:bottom;padding-bottom:50px">
+              <v-row>
+                  <v-col>
+                      <h1 class="white--text font-weight-light text-right">
+                          1020hp
+                      </h1>
+                      <p class="white--text text-right">Peak Power</p>
+                  </v-col>
+                   <v-col>
+                      <h1 class="white--text font-weight-light text-center">
+                          9.9s
+                      </h1>
+                      <p class="white--text text-center">1/4 mile</p>
+                  </v-col>
+                   <v-col>
+                      <h1 class="white--text font-weight-light">
+                          2.5s
+                      </h1>
+                      <p class="white--text">0-60 mph*</p>
+                  </v-col>
+              </v-row>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .background {
+        background: url('../../public/tesla-specs.jpeg');
+        background-size:cover;
+        width: 100%;
+        height:70vh;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,23 @@
   <div>
      <Promo/>
   <Specifications/>
+  </Interior>
+  <CoreSpecs/>
   </div>
 </template>
 
 <script>
   import Promo from '../components/Promo.vue'
-
   import Specifications from '../components/Specifications.vue'
+  import Interior from '../components/Interior.vue'
+  import CoreSpecs from '../components/CoreSpecs.vue'
 export default {
   name: 'HomeView',
   components: {
     Promo,
-    Specifications
+    Specifications,
+    Interior,
+    CoreSpecs
   }
 }
 </script>
